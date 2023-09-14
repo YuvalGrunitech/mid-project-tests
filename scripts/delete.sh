297e2445e599229dc8947c72077f9720601718e1
jenkins <yuvalaws12@gmail.com>
2023-09-14 11:29:46 +0300

    delete.sh fixed

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -7,6 +7,6 @@ else
     VERSION=$1
 fi 
 
-gcloud compute ssh --zone "me-west1-a" "yuval-instance" --project "deductive-jet-389317" --command "docker rm -f chat-app-cont && docker rmi -f me-west1-docker.pkg.dev/grunitech-mid-project/yuval-chat-app/chat-app-img:$VERSION"
+gcloud compute ssh --zone "me-west1-a" "yuval-first-instance" --project "grunitech-mid-project" --command "docker rm -f chat-app-cont && docker rmi -f me-west1-docker.pkg.dev/grunitech-mid-project/yuval-chat-app/chat-app-img:$VERSION"
 
 }
